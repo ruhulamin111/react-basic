58acbe05777d9660522c18109e0b40ce42cd4acd
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-19 20:02:14 +0600

    send data with props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 
 function App() {
+
   return (
     <div>
-      <Shop></Shop>
+      <Shop name="abc" age="12"></Shop>
+      <Shop name='def' age='25'></Shop>
 
     </div>
   );
 }
 
-function Shop() {
+function Shop(props) {
+
   return (
     <div className='shop'>
+      <p>{props.name}</p>
+      <p>{props.age}</p>
       <h1>Beliver Sign</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam necessitatibus perspiciatis repellat rem saepe tempore. Distinctio quod minus magnam tenetur.</p>
     </div>
