1f2bf306edb416d3474408293a0c8128b945f4ff
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-19 21:37:14 +0600

    array of object dynamic load

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
 function App() {
-  const products = ['chips', 'ice cream', 'chocolet', 'biscuit']
+  const products = ['chips', 'ice cream', 'chocolet', 'biscuit', 'burger']
+  const students = [
+    { name: 'abu bakar', age: 34 },
+    { name: 'abdul karim', age: 44 },
+    { name: 'baten shah', age: 35 },
+    { name: 'shahen shah', age: 29 },
+    { name: 'niyamot ullah', age: 38 }
+  ]
   return (
     <div>
       {
         products.map(product => <Shop name={product}></Shop>)
+      }
+      {
+        students.map(student => <Seven
+          name={student.name}
+          age={student.age}
+        ></Seven>)
       }
 
       {/* <Shop name='def' age='25'></Shop> */}
@@ -13,9 +26,16 @@ function App() {
     </div>
   );
 }
+function Seven(props) {
+  return (
+    <div className='school'>
+      <h2>Name: {props.name} </h2>
+      <h3>Age: {props.age}</h3>
+    </div>
+  )
+}
 
 function Shop(props) {
-
   return (
     <div className='shop'>
       <h1>Beliver Sign</h1>
